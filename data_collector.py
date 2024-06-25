7387a6db33b64d18cebe891c293ed27f1a96d0f5
Paul Wrubel <pwrubel7@gmail.com>
2024-06-24 22:17:43 -0500

    Improve xnb hack script

--- data_collector.py
+++ data_collector.py
@@ -1,39 +1,99 @@
 import shutil
 import subprocess
+import sys
 import requests
 import zipfile
 import os
 
-MACOS_DEFAULT_PATH="/Users/madelineleclair/Library/Application Support/Steam/steamapps/common/Stardew Valley/Contents/MacOS/"
-BINARY_EXTRACT_DIRECTORY="./xnb_hack"
-STARDEW_XNB_HACK="https://github.com/Pathoschild/StardewXnbHack/releases/download/1.1.0/StardewXnbHack-1.1.0-for-macOS.zip"
-XNB_HACK_ZIP="xnb_hack.zip"
-XNB_HACK_BINARY="xnb_hack/StardewXnbHack 1.1.0 for macOS/StardewXnbHack"
-NEW_XNB_HACK=MACOS_DEFAULT_PATH+"StardewXnbHack"
+XNB_HACK_VERSION = "1.1.0"
+# NEW_XNB_HACK = MACOS_DEFAULT_PATH+"StardewXnbHack"
 
 def main():
-    # Download latest version of StardewXnbHack
-    response = requests.get(STARDEW_XNB_HACK)
-    with open(XNB_HACK_ZIP, "xb") as xnb_hack:
-        xnb_hack.write(response.content)
+
+    unpacked_dir = f"{get_game_install_dir()}/Content (unpacked)"
+    if os.path.exists(unpacked_dir):
+        print("Content is already unpacked! Do you want to continue? (Y/n)")
+        yn_input = input()
+        if yn_input.lower() != "y":
+            print("Exiting...")
+            exit()
+
+    # Find current platform
+    if sys.platform.startswith("darwin"):
+        platform_string = "macOS"
+    elif sys.platform.startswith("linux"):
+        platform_string = "Linux"
+    else:
+        raise Exception(f"Unsupported platform: {sys.platform}")
     
-    os.makedirs(BINARY_EXTRACT_DIRECTORY, exist_ok=True)
+    # Download latest version of StardewXnbHack for current platform
+    print(f"Downloading StardewXnbHack for {platform_string}...")
+    response = requests.get(f"https://github.com/Pathoschild/StardewXnbHack/releases/download/{XNB_HACK_VERSION}/StardewXnbHack-{XNB_HACK_VERSION}-for-{platform_string}.zip")
+    if response.status_code != 200:
+        raise Exception(f"Failed to download StardewXnbHack. Response code: {response.status_code}")
+
+    # Save downloaded zip to temporary directory
+    if os.path.exists("./tmp"):
+        print("Removing old ./tmp directory...")
+        shutil.rmtree("./tmp")
+        
+    print("Creating ./tmp directory...")
+    os.mkdir("./tmp")
+
+    with open("./tmp/xnb_hack.zip", "w+b") as xnb_hack:
+        print("Writing downloaded zip to ./tmp/xnb_hack.zip ...")
+        xnb_hack.write(response.content)
 
     # Unzip StardewXnbHack
-    with open(XNB_HACK_ZIP, "rb") as zip_file:
+    with open("./tmp/xnb_hack.zip", "rb") as zip_file:
         with zipfile.ZipFile(zip_file, "r") as zip:
-            zip.extractall(BINARY_EXTRACT_DIRECTORY)
+            print("Unzipping ./tmp/xnb_hack.zip to ./tmp/xnb_hack/ ...")
+            zip.extractall("./tmp/xnb_hack")
 
-    # Move binary to game install directory     
-    shutil.move(XNB_HACK_BINARY, NEW_XNB_HACK)
+    # Move binary to game install directory
+    downloaded_binary_path = f"./tmp/xnb_hack/StardewXnbHack {XNB_HACK_VERSION} for {platform_string}/StardewXnbHack"
+    installed_binary_path = f"{get_game_install_dir()}/StardewXnbHack"
+
+    try:
+        if os.path.exists(installed_binary_path):
+            print("Removing old StardewXnbHack...")
+            os.remove(installed_binary_path)
+
+        print(f"Moving {downloaded_binary_path} to {installed_binary_path} ...")
+        shutil.move(downloaded_binary_path, installed_binary_path)
+        os.chmod(installed_binary_path, 0o755) # chmod +x
+    except Exception as e:
+        raise Exception(f"Failed to move StardewXnbHack. Error: {e}")
+    
 
     # Run StardewXnbHack
-    os.chmod(NEW_XNB_HACK, 755)
-    subprocess.call(NEW_XNB_HACK)
+    print("Running StardewXnbHack...")
+    code = subprocess.call(installed_binary_path, cwd="./tmp")
+    if code != 0:
+        raise Exception(f"Failed to run StardewXnbHack. Exit code: {code}")
+    
+    # General cleanup
+    print("Removing ./tmp directory...")
+    shutil.rmtree("./tmp")
 
     # Translate resources into readable format -- Parsing Script
+    print("[TODO] Translating resources...")
+    # TODO: Implement
 
     # Deposit output into iOS + Android Projects
+    print("[TODO] Depositing output into iOS + Android Projects...")
+    # TODO: Implement
+
+def get_game_install_dir():
+    GAME_INSTALL_DIR_MACOS="~/Library/Application Support/Steam/steamapps/common/Stardew Valley/Contents/MacOS"
+    GAME_INSTALL_DIR_LINUX="~/.steam/steam/steamapps/common/Stardew Valley"
+
+    if sys.platform.startswith("darwin"):
+        return os.path.expanduser(GAME_INSTALL_DIR_MACOS)
+    elif sys.platform.startswith("linux"):
+        return os.path.expanduser(GAME_INSTALL_DIR_LINUX)
+    else:
+        raise Exception(f"Unsupported platform: {sys.platform}")
 
 if __name__ == "__main__":
     main()
